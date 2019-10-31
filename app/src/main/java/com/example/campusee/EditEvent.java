4c0f6dbfb5944019f407ec98d42427ed84273d23
Stacy <stacy.phan@joinhoney.com>
2019-10-31 16:13:25 -0700

    Grab a single event.

--- app/src/main/java/com/example/campusee/EditEvent.java
+++ app/src/main/java/com/example/campusee/EditEvent.java
@@ -3,9 +3,13 @@ package com.example.campusee;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 public class EditEvent extends AppCompatActivity {
 
@@ -39,5 +43,31 @@ public class EditEvent extends AppCompatActivity {
             publisherEventDatabaseRef.child("status").setValue(false);
         }
 
+    }
+
+    private void seeEventDetails(String eventId) {
+        DatabaseReference eventDatabaseRef = mDatabase.child("events").child(eventId);
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Event event = dataSnapshot.getValue(Event.class);
+
+                // TODO: Update UI
+                /* E.g. from firebase:
+                    mAuthorView.setText(post.author);
+                    mTitleView.setText(post.title);
+                    mBodyView.setText(post.body);
+                */
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.e("LoadEvent error", "event:onCancelled:" + databaseError.getMessage());
+            }
+        };
+        eventDatabaseRef.addValueEventListener(eventListener);
     }
 }
