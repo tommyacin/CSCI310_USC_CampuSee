57d53503f781cb29ac06287cc923a3a324fd2b44
Stacy <stacy.phan@joinhoney.com>
2019-10-30 21:39:39 -0700

    Created Publisher and User write functions.

--- app/src/main/java/com/example/campusee/MainActivity.java
+++ app/src/main/java/com/example/campusee/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.campusee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting the database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Button studentButton = (Button) findViewById(R.id.student_button);
         studentButton.setOnClickListener(new View.OnClickListener() {
@@ -34,4 +43,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Write user to database
+    private void writeNewPerson(String email, String password) {
+        User user = new User(email, password);
+
+        String key = mDatabase.child("users").push().getKey();
+        mDatabase.child("users").child(key).setValue(user);
+    }
+
+    private void writeNewPublisher(String email, String password) {
+        Publisher publisher = new Publisher(email, password);
+
+        String key = mDatabase.child("publishers").push().getKey();
+        mDatabase.child("publishers").child(key).setValue(publisher);
+    }
+
 }
---
+++ app/src/main/java/com/example/campusee/Publisher.java
@@ -0,0 +1,13 @@
+package com.example.campusee;
+
+public class Publisher {
+    public String email;
+    public String password;
+
+    Publisher() { }
+
+    Publisher(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
