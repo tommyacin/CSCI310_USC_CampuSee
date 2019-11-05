d4c1b3a45123fc1f874a875d8ebda1ac02c126c0
Tomas Acin <acinched@usc.edu>
2019-11-04 23:30:31 -0800

    publisher sign up and login connected with database

--- app/src/main/java/com/example/campusee/PublisherSignup.java
+++ app/src/main/java/com/example/campusee/PublisherSignup.java
@@ -2,19 +2,26 @@ package com.example.campusee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class PublisherSignup extends AppCompatActivity {
-
     private DatabaseReference mDatabase;
+    private String mPublisherID;
+    private boolean mPublisherExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +30,30 @@ public class PublisherSignup extends AppCompatActivity {
 
         // Getting the database
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        Button publisherButton = (Button) findViewById(R.id.next_button);
-        publisherButton.setOnClickListener(new View.OnClickListener() {
+        mPublisherID = null;
+        mPublisherExists = false;
+
+        //sign up
+        Button publisherSignUpButton = (Button) findViewById(R.id.publisher_signup_button);
+        publisherSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // add code here for what will happen when the user selects the student button
-                EditText name_input = (EditText) findViewById(R.id.publisher_signup_name);
-                EditText username_input = (EditText) findViewById(R.id.publisher_signup_username);
-                EditText email_input = (EditText) findViewById(R.id.publisher_signup_email);
-                EditText building_input = (EditText) findViewById(R.id.publisher_signup_building);
+                String publisherID = buttonClick();
 
-                String name = name_input.getText().toString();
-                String username = username_input.getText().toString();
-                String email = email_input.getText().toString();
-                String building = building_input.getText().toString();
+                Intent publisherIntent = new Intent(getApplicationContext(), PublisherMain.class);
+                publisherIntent.putExtra("currentPublisherID", publisherID);
+                PublisherSignup.this.startActivity(publisherIntent);
+            }
+        });
 
-                String publisherID = writeNewPublisher(email, "hello", building);
+        //login
+        Button publisherLoginButton = (Button) findViewById(R.id.publisher_login_button);
+        publisherLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // add code here for what will happen when the user selects the student button
+                String publisherID = buttonClick();
 
                 Intent publisherIntent = new Intent(getApplicationContext(), PublisherMain.class);
                 publisherIntent.putExtra("currentPublisherID", publisherID);
@@ -47,11 +62,52 @@ public class PublisherSignup extends AppCompatActivity {
         });
     }
 
-    //write new publisher to database; return unique publisherID
-    private String writeNewPublisher(String email, String password, String building) {
-        Publisher publisher = new Publisher(email, password, building);
-        Log.d("write_new_publisher", "building: " + building);
+    private String buttonClick() {
+        EditText name_input = (EditText) findViewById(R.id.publisher_signup_name);
+        EditText email_input = (EditText) findViewById(R.id.publisher_signup_email);
+        EditText password_input = (EditText) findViewById(R.id.publisher_signup_password);
+        EditText building_input = (EditText) findViewById(R.id.publisher_signup_building);
 
+        String name = name_input.getText().toString();
+        String password = password_input.getText().toString();
+        String email = email_input.getText().toString();
+        String building = building_input.getText().toString();
+
+        return publisherSignupOrLogin(email, password, building);
+    }
+
+    private String publisherSignupOrLogin(String email, String password, String building) {
+        //check user exists
+        DatabaseReference publishersRef = mDatabase.child("publishers");
+        Query emailQuery = publishersRef.orderByChild("email").equalTo(email);
+        emailQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot  publisher: dataSnapshot.getChildren() ){
+                    // Iterate through all the publishers with the same email
+                    publisher.getKey();
+                    mPublisherID = publisher.getKey();
+                    mPublisherExists = true;
+                    Log.d("isExistingpublisher", mPublisherID);
+                    return;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //if exists, return that publisherID
+        if (mPublisherExists) {
+            Log.d("isExisting", "here: " + mPublisherID);
+            return mPublisherID;
+        }
+
+        //else, create new publisher
+        Publisher publisher = new Publisher(email, password, building);
+        Log.d("write_new_publisher", "email + building: " + email + " " + building);
         String key = mDatabase.child("publishers").push().getKey();
         mDatabase.child("publishers").child(key).setValue(publisher);
         return key;
