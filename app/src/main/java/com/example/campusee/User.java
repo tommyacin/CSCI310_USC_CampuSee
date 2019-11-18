282533f1f3ccab1940719f75e8fdef3b0951a570
Tomas Acin <acinched@usc.edu>
2019-11-17 19:54:09 -0800

    added tests for User object

--- app/src/main/java/com/example/campusee/User.java
+++ app/src/main/java/com/example/campusee/User.java
@@ -1,32 +1,13 @@
 package com.example.campusee;
 
-
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import java.util.ArrayList;
-import java.util.List;
-import android.content.Context;
-
-import java.util.Vector;
-
 public class User {
     public String name;
     public String email;
     public String password;
 
-    private List<Notification> notifications;
-
-    User() { }
-
     User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.notifications = new ArrayList<>();
-    }
-
-    public List<Notification> getNotifications(){
-        return notifications;
     }
 }
