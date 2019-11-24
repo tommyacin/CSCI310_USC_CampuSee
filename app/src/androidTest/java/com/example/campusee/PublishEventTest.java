88e148376549f5b8ee2842bc137a403e87828916
Nicholas Nguyen <Nicholas@nicholass-mbp-2.home>
2019-11-24 00:01:58 -0800

    wip

--- app/src/androidTest/java/com/example/campusee/PublishEventTest.java
+++ app/src/androidTest/java/com/example/campusee/PublishEventTest.java
@@ -1,6 +1,8 @@
 package com.example.campusee;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SdkSuppress;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -12,21 +14,45 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
 
 @RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
 public class PublishEventTest {
+    private UiDevice device;
+
     @Rule
-    public ActivityTestRule<PublishEvent> publishEventActivityTestRule =
-            new ActivityTestRule<>(PublishEvent.class, true, true);
+    public ActivityTestRule<CreateEvent> createEventActivityTestRule =
+            new ActivityTestRule<>(CreateEvent.class, true, true);
+
+
 
     @Test
     public void createEventTest() {
-        onView(withId(R.id.publish_event_name))
-                .perform(typeText("Ginger Dudley"), closeSoftKeyboard()); //type email and hide keyboard
-        onView(withId(R.id.publisher_signup_email))
-                .perform(typeText("ginger@gmail.com"), closeSoftKeyboard());//type password and hide keyboard
-        onView(withId(R.id.publisher_signup_password))
-                .perform(typeText("ginger"), closeSoftKeyboard());//type password and hide keyboard*/
+        // Initialize UiDevice instance
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        String eventTitle = "Test Event";
+        String eventDate = "01/02/2019";
+        String eventTime = "11:00";
+        String eventDes = "Testing event";
+        String eventRadius = "2";
+
+        UiObject date_widget = device.findObject(new UiSelector().resourceId("create_datepicker"));
+
+        onView(withId(R.id.create_event_name))
+                .perform(typeText(eventTitle), closeSoftKeyboard()); //type email and hide keyboard
+        onView(withId(R.id.publish_date))
+                .perform(typeText(eventDate), closeSoftKeyboard());//type password and hide keyboard
+        onView(withId(R.id.publish_time))
+                .perform(typeText(eventTime), closeSoftKeyboard());//type password and hide keyboard*/
+        onView(withId(R.id.create_radius)).perform(typeText(eventRadius), closeSoftKeyboard());
+        onView(withId(R.id.create_description)).perform(typeText(eventDes), closeSoftKeyboard());
+
         onView(withId(R.id.publisher_signup_button)).perform(click()); //perform click
     }
 }
--- app/src/androidTest/java/com/example/campusee/PublisherPageOfEventsTest.java
+++ app/src/androidTest/java/com/example/campusee/PublisherPageOfEventsTest.java
@@ -1,6 +1,5 @@
 package com.example.campusee;
 
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -10,8 +9,6 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
